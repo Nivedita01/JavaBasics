afbd05db96d26cf574bfec4766a43b1d5da5862f
Nivedita <hari.nivedita@gmail.com>
2017-04-21 10:26:49 -0400

    Class demo using string args

---
+++ ClassDemo.java
@@ -0,0 +1,11 @@
+package javaTutorial;
+
+public class ClassDemo {
+
+	public static void main(String[] args) {
+		for(int i=0; i<args.length;i++)
+			System.out.println("Arguments in the string are ::"+args[i]);
+
+	}
+
+}
